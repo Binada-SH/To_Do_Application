655578cc0c97ecca1b80188866a8f6d7225afaac
Binada-SH <binadasasmika@gmail.com>
2025-04-03 22:17:42 +0530

    Home page creaion and welcome msg print

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_application/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }

4f6e0d2b30c71856e21a4273b609fba238d982f6
Binada-SH <binadasasmika@gmail.com>
2025-04-04 21:20:35 +0530

    Splash Screen intergration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_application/pages/home.dart';
 
-void main() {
+Future<void>main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(Duration(milliseconds: 400));
   runApp(const MyApp());
 }
 

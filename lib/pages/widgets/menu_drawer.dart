15254a79e36bc5337bf0d76d0d191e813e33ea72
Binada-SH <binadasasmika@gmail.com>
2025-04-07 20:06:06 +0530

    MWenu drawer items adding

--- lib/pages/widgets/menu_drawer.dart
+++ lib/pages/widgets/menu_drawer.dart
@@ -14,13 +14,13 @@ class menuDrawer extends StatelessWidget {
         )
       ),
       child: Container(
-        color: Colors.white,
+        color: Color(0xFFAAE9E9),
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             const DrawerHeader(
               decoration: BoxDecoration(
-                color: Color(0xFFCDE8E5),
+                color: Color(0xFFAAE9E9),
                 ),
                 child: Padding(
                   padding: EdgeInsets.only(top: 43, left: 40),
@@ -28,26 +28,53 @@ class menuDrawer extends StatelessWidget {
                     'Menu',
                     style: TextStyle(
                       fontFamily: 'Jura',
-                      fontWeight: FontWeight.w800,
                       letterSpacing: 1.3,
                       fontSize: 26,
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
                 ),
               ),
               ListTile (
+                leading: Icon(
+                  Icons.home,
+                  ),
                   title: Text(
                   'Home',
                   style: TextStyle(
                     fontFamily: 'Jura',
                     letterSpacing: 1.3,
-                    fontWeight: FontWeight.w800,
-
+                    fontSize: 17,
+                    fontWeight: FontWeight.w900,
                   ),
                 ),
-                  
-                  onTap: (){},
+                onTap: (){},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text(
+                  "Settings",
+                  style: TextStyle(
+                    fontFamily: 'Jura',
+                    letterSpacing: 1.3,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w900,
+                  ),
                 ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  'Account',
+                  style: TextStyle(
+                    fontFamily: 'Jura',
+                    letterSpacing: 1.3,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w900
+                  ),
+                ),
+              )
+              
           ],
         )
       ),

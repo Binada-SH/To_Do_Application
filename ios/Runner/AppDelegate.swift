ddd3559b161f2429ea431e99f81f01ab3177b6ae
Binada-SH <binadasasmika@gmail.com>
2025-04-11 23:25:51 +0530

    Unnecessary Platform File Deletion

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import flutter_local_notifications // Add this import
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,19 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // Request notification permissions (needed for iOS 10+)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
+    // ---- Add this part ----
+    // This is required to make any communication available in the action isolate.
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+    // ---- End of added part ----
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
